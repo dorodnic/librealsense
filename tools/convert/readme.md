f79b0c561a3e6dd52ffe3caf96b9ff88ed7b7cf3
Sergey Dorodnicov <sergey.dorodnicov@intel.com>
2018-04-12 15:46:07 +0300

    First commit for rs-convert

---
+++ tools/convert/readme.md
@@ -0,0 +1,8 @@
+# rs-convert Tool
+
+## Goal
+`rs-convert` is intended to be used to convert data from RealSense devices / recordings into various different file formats.
+
+## Usage
+
+TBD
---
+++ tools/convert/rs-convert.cpp
@@ -0,0 +1,22 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2018 Intel Corporation. All Rights Reserved.
+
+#include <librealsense2/rs.hpp>
+#include <iostream>
+#include <fstream>
+
+#include "tclap/CmdLine.h"
+
+#include <string>
+
+
+using namespace std;
+using namespace TCLAP;
+
+int main(int argc, char** argv)
+{
+    CmdLine cmd("librealsense rs-convert tool", ' ', RS2_API_VERSION_STR);
+    
+    
+}
+
